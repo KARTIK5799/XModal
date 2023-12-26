a00bbfe80b66c52f2bdf5936e4c58648ff610031
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-26 18:39:15 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,24 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import User from "./components/User";
 
 const App = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
-    setModalOpen(true);
+    setIsOpen(true);
   };
 
   const closeModal = () => {
-    setModalOpen(false);
-  };
-
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    phone: '',
-    dob: '',
-  });
-
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({
-      ...formData,
-      [id]: value,
-    });
-
-    e.target.setCustomValidity('');
-
-    if (id === 'email') {
-      if (value && !value.includes('@')) {
-        e.target.setCustomValidity(`Please include an '@' in the email address. '${value}' is missing an '@'.`);
-      }
-    }
-  };
-
-  const handleSubmit = () => {
-    if (!/^\d{10}$/.test(formData.phone)) {
-      alert('Invalid phone number. Please enter a 10-digit phone number.');
-      return;
-    }
-
-    const currentDate = new Date();
-    const enteredDate = new Date(formData.dob);
-
-    if (enteredDate > currentDate) {
-      alert('Invalid date of birth. Date of birth cannot be in the future.');
-      return;
-    }
-
-    console.log(formData);
-    closeModal();
-    resetForm(); 
-  };
-
-  const resetForm = () => {
-    setFormData({
-      username: '',
-      email: '',
-      phone: '',
-      dob: '',
-    });
-  };
-
-  const handleModalClick = (e) => {
-    if (e.target.classList.contains('modal')) {
-      closeModal();
-      resetForm(); 
-    }
+    setIsOpen(false);
   };
 
   return (
-    <div className='app'>
+    <div className="App">
       <h1>User Details Modal</h1>
-      <button onClick={openModal}>Open Form</button>
-
-      {isModalOpen && (
-        <div className="modal" onClick={handleModalClick}>
-          <div className="modal-content">
-            <div>
-              <form onSubmit={handleSubmit}>
-                <h2>Fill Details</h2>
-                <label htmlFor="username">Username:</label>
-                <input
-                  type="text"
-                  id="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  required
-                />
-
-                <label htmlFor="email">Email Address:</label>
-                <input
-                  type="email"
-                  id="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-
-                <label htmlFor="phone">Phone Number:</label>
-                <input
-                  type="tel"
-                  id="phone"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  required
-                />
-
-                <label htmlFor="dob">Date of Birth:</label>
-                <input
-                  type="date"
-                  id="dob"
-                  value={formData.dob}
-                  onChange={handleChange}
-                  required
-                />
-
-                <button className="submit-button" type="submit">
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      )}
+      <button onClick={openModal} className="submit-button">
+        Open Form
+      </button>
+      {isOpen && <User closeModal={closeModal} />}
     </div>
   );
 };
