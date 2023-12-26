62b4e39af27623e779899da37a4087b05c750685
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-26 14:06:29 +0530

    form containing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+// App.js
 import React, { useState } from 'react';
-import Form from './components/Form';
-import './App.css'
+
+import './App.css';
 
 const App = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -11,6 +12,49 @@ const App = () => {
 
   const closeModal = () => {
     setModalOpen(false);
+  };
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    phone: '',
+    dob: '',
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+
+    e.target.setCustomValidity('');
+
+    if (id === 'email') {
+      if (value && !value.includes('@')) {
+        e.target.setCustomValidity(`Please include an '@' in the email address. '${value}' is missing an '@'.`);
+      }
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!/^\d{10}$/.test(formData.phone)) {
+      alert('Invalid phone number. Please enter a 10-digit phone number.');
+      return;
+    }
+
+    const currentDate = new Date();
+    const enteredDate = new Date(formData.dob);
+
+    if (enteredDate > currentDate) {
+      alert('Invalid date of birth. Date of birth cannot be in the future.');
+      return;
+    }
+
+    console.log(formData);
+    closeModal();
   };
 
   const handleModalClick = (e) => {
@@ -27,7 +71,50 @@ const App = () => {
       {isModalOpen && (
         <div className="modal" onClick={handleModalClick}>
           <div className="modal-content">
-           <Form/>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <h2>Fill Details</h2>
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                />
+
+                <label htmlFor="email">Email Address:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+
+                <label htmlFor="phone">Phone Number:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                />
+
+                <label htmlFor="dob">Date of Birth:</label>
+                <input
+                  type="date"
+                  id="dob"
+                  value={formData.dob}
+                  onChange={handleChange}
+                  required
+                />
+
+                <button className="submit-button" type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       )}
